0029ad61992ef35033f205043497f5de49adf69f
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-06 20:40:11 +0100

    Update codebase.js

--- codebase.js
+++ codebase.js
@@ -33,3 +33,4 @@ var mark = {
         this.finalPaid[i] = this.bills[i] + this.allTips[i];
       }else if (this.bills[i] >= 100 && this.bills[i] <= 300){
         this.allTips[i] = this.bills[i] * 0.1;
+        this.finalPaid[i] = this.bills[i] + this.allTips[i];
